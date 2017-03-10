a2bb2d290b94a74e2128d55937b5b7ce67cd528e
RUSSEL VIOLA <RUSSELVIOLA@GMAIL.COM>
2017-03-10 18:20:23 -0500

    added loaders to webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,17 @@ module.exports = {
   },
   resolve: {
     extentions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        },
+        tests: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
   }
 };
