453a7f858441445c17b01eb48904c7c5ed8e79b3
RUSSEL VIOLA <RUSSELVIOLA@GMAIL.COM>
2017-03-23 13:34:24 -0400

    forgot to wrap About elements in div

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,10 +3,12 @@ import React, { Component } from 'react';
 const About = props => {
 
   return (
-    <h3>About</h3>
-    <p>This Application was created using React.js and NodeJS</p>
-    <p>Powered by OpenWeatherMap API</p>
-    <p>Created by: Russel Viola</p>
+    <div>
+      <h3>About</h3>
+      <p>This Application was created using React.js and NodeJS</p>
+      <p>Powered by OpenWeatherMap API</p>
+      <p>Created by: Russel Viola</p>
+    </div>
   )
 }
 
