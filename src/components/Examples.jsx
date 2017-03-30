0479da5a05402dae79af4629e3fe052cae1b28ab
RUSSEL VIOLA <RUSSELVIOLA@GMAIL.COM>
2017-03-30 14:14:44 -0400

    columnns sizing, centering, and dummy links

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import {Link} from "react-router";
 
 const Examples = props => {
 
   return (
-    <h3>Examples</h3>
+    <div>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations to try out:</p>
+      <ol>
+        <li>
+          <Link to='/?location=Boston'>Boston, Ma</Link>
+        </li>
+        <li>
+          <Link to="/?location=Rio">Rio, Brazil</Link>
+        </li>
+      </ol>
+    </div>
   )
 }
 
