f42004ba83a4206bfdab6d201f464c35ff9193fa
RUSSEL VIOLA <RUSSELVIOLA@GMAIL.COM>
2017-03-31 15:28:45 -0400

    button restyling

--- src/components/WeatherForm.jsx
+++ src/components/WeatherForm.jsx
@@ -16,7 +16,7 @@ export default class WeatherForm extends Component {
       <div>
         <form onSubmit={this.onFormSubmit.bind(this)}>
           <input type="text" ref="location"/>
-          <button>Get Weather</button>
+          <button className="button expanded hollow">Get Weather</button>
         </form>
       </div>
     )
