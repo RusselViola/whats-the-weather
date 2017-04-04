ccd39a9114b853f35edf5bd16726cf6bd5ab1a21
RUSSEL VIOLA <RUSSELVIOLA@GMAIL.COM>
2017-04-04 12:41:07 -0400

    custome modal done except for one bug

--- src/components/ErrorModal.jsx
+++ src/components/ErrorModal.jsx
@@ -2,17 +2,30 @@ import React, { Component } from 'react';
 
 export default class ErrorModal extends Component {
 
+  static defaultProps() {
+    return {
+      title: 'Error'
+    }
+  }
+
+  propTypes: {
+    title: React.PropTypes.string,
+    message: React.PropTypes.string.isRequired
+  }
+
   componentDidMount() {
     let modal = new Foundation.Reveal($('#error-modal'));
     modal.open();
   }
 
   render() {
+    let {title, message} = this.props;
+    debugger;
     return (
 
         <div id="error-modal" className="reveal tiny text-center" data-reveal="">
-          <h4>Modal Message</h4>
-          <p>Error message</p>
+          <h4>{title}</h4>
+          <p>{message}</p>
           <p>
             <button className="button hollow" data-close="">
               OK
